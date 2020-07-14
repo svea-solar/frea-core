bd0f57ef448adefc03c2035e813e63bad6a64957
Marcus Rådell <marcus.radell@sveasolar.com>
2020-07-14 18:44:43 +0200

    fix export

--- src/drivers/http/index.ts
+++ src/drivers/http/index.ts
@@ -1,7 +1,7 @@
 import { getEnv } from "../../get_env";
 import { createApi } from "./api";
 
-export const create = () => {
+export const createHttpDriver = () => {
   const port = getEnv("PORT");
   const api = createApi({ port });
 
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export * from "./get_env";
+export * from "./drivers";
