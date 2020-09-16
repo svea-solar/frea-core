dc02b5cc713b0422641babd3c914166c772076ce
Marcus Rådell <marcus.radell@sveasolar.com>
2020-09-16 21:35:39 +0200

    add ApiResult and deprecate Result

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
+// Deprecated
 export type Ok<T> = T extends void
   ? { status: "succeeded" }
   : {
@@ -5,11 +6,25 @@ export type Ok<T> = T extends void
       data: T;
     };
 
+// Deprecated
 export type BaseErr = { reason: string };
 
+// Deprecated
 export type Err<E extends BaseErr> = {
   status: "failed";
   error: E;
 };
 
+// Deprecated
 export type Result<T, E extends BaseErr> = Promise<Ok<T> | Err<E>>;
+
+export type ApiOk<T> = T extends void ? { ok: true } : { ok: true; data: T };
+
+export type ApiErr<E extends { reason: string }> = {
+  ok: false;
+  error: E;
+};
+
+export type ApiResult<T, E extends { reason: string }> = Promise<
+  ApiOk<T> | ApiErr<E>
+>;
