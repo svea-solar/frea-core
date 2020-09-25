45ac597d12067a3d75a4fb43f505e0bb41a12209
Marcus Rådell <marcus.radell@sveasolar.com>
2020-09-25 13:10:24 +0200

    breaking changes to the http driver to support cors list and prefix the env vars

--- src/drivers/http/index.ts
+++ src/drivers/http/index.ts
@@ -3,8 +3,10 @@ import { createApi } from "./api";
 export * from "./types";
 
 export const create = () => {
-  const port = getEnv("PORT");
-  const api = createApi({ port });
+  const port = getEnv("HTTP_DRIVER_PORT");
+  const corsList = getEnv("HTTP_DRIVER_CORS_LIST").split(",");
+
+  const api = createApi({ port, corsList });
 
   return api;
 };
