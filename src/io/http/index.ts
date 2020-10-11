674f94688a96e74579c2409ba45a3b7e75e4119f
Marcus Rådell <marcus.radell@sveasolar.com>
2020-10-11 23:30:07 +0200

    fix http export

--- src/io/http/index.ts
+++ src/io/http/index.ts
@@ -1,8 +1,11 @@
 import { getEnv } from "../../get_env";
 import { createApi } from "./api";
+import { HttpApi } from "./types";
 export * from "./types";
 
-export const create = () => {
+type Create = () => Promise<HttpApi>;
+
+export const createHttp: Create = () => {
   const port = getEnv("HTTP_DRIVER_PORT");
   const corsList = getEnv("HTTP_DRIVER_CORS_LIST").split(",");
 
