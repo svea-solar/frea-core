2a0725443e75a9f283dc8eba9e2368a71f682297
Marcus Rådell <marcus.radell@sveasolar.com>
2020-10-17 10:54:56 +0200

    change env vars to args

--- src/io/http/index.ts
+++ src/io/http/index.ts
@@ -1,16 +1,16 @@
 import { JwtAdapter } from "adapters";
-import { getEnv } from "../../get_env";
 import { createMod } from "./mod";
 import { HttpMod } from "./types";
 export * from "./types";
 
 export const createHttpIo = <TToken>({
   jwt,
+  port,
+  corsList,
 }: {
   jwt: JwtAdapter<TToken>;
+  port: string;
+  corsList: string[];
 }): Promise<HttpMod> => {
-  const port = getEnv("HTTP_DRIVER_PORT");
-  const corsList = getEnv("HTTP_DRIVER_CORS_LIST").split(",");
-
   return createMod({ port, corsList, jwt });
 };
