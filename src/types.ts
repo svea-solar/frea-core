a67b6123f532175668dba7dfc9dc6ac4eb95c838
Bawer Dagdeviren <bawerd@gmail.com>
2021-01-14 14:58:25 +0100

    moved types

--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,49 @@ export type Err<E extends { code: string }> = {
 };
 
 export type Result<T, E extends { code: string }> = Promise<Ok<T> | Err<E>>;
+
+export type Severity = "info" | "error";
+
+export type Log = (args: {
+  mod: string;
+  type: string;
+  severity: Severity;
+  traceId?: string;
+  result: unknown;
+  clientCid?: string;
+  args: unknown;
+}) => void;
+
+export type ActionArgsSchema<
+  TAction extends (args: any, ctx: any) => Result<any, any>
+  > = TAction extends (args: infer TArgs, ctx: any) => Result<any, any>
+  ? (keyof TArgs)[]
+  : never;
+
+export type Ctx = {
+  type: string;
+  token: any;
+  clientCid?: string;
+  clientIp?: string;
+};
+
+export type Mod = {
+  [k: string]: (args: any, ctx: Ctx) => Result<any, any>;
+};
+
+export type ActionsSchema<TApi extends Mod> = {
+  [K in keyof TApi]: {
+    args: ActionArgsSchema<TApi[K]>;
+    public?: true;
+  };
+};
+
+export type ModuleSchema<TApi extends Mod> = {
+  module: string;
+  actions: ActionsSchema<TApi>;
+};
+
+export type ActionSchema = {
+  args: string[];
+};
+
