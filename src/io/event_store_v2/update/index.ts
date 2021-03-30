80f4cd5421de3c8f54ddfd7402e4256445290a72
Marcus Rådell <marcus.radell@sveasolar.com>
2021-03-30 14:04:56 +0200

    Refactor event store update to its own folder.

---
+++ src/io/event_store_v2/update/index.ts
@@ -0,0 +1,65 @@
+import pgPromise from "pg-promise";
+import { Err, Ok } from "../../..";
+import { Update, UpdateErr, UpdateOk } from "./types";
+
+export const createUpdate = ({
+  db,
+  name,
+}: {
+  db: pgPromise.IDatabase<unknown>;
+  name: string;
+}): Update => async ({ idKey, idVal, event, reducer }) =>
+  db
+    .tx(async (t) => {
+      const selectResult = await t.oneOrNone(
+        /*sql*/ `SELECT * FROM $<name:name>.cache WHERE data->>$<idKey> = $<idVal>`,
+        { idKey, idVal, name }
+      );
+
+      if (!selectResult) {
+        const err: Err<UpdateErr> = {
+          ok: false,
+          error: {
+            code: "io/event_store.update->failed:item_not_found",
+            args: { idKey, idVal },
+          },
+        };
+        return err;
+      }
+
+      const currentState = selectResult.data;
+
+      const nextState = reducer(currentState, event);
+
+      await t.none(
+        /*sql*/ `INSERT INTO $<name:name>.events
+        (event)
+        VALUES
+        ($<event>)`,
+        {
+          name,
+          event,
+        }
+      );
+
+      await t.none(
+        /*sql*/ `UPDATE $<name:name>.cache SET data = $<data> where uuid = $<uuid>`,
+        {
+          name,
+          uuid: event.uuid,
+          data: nextState,
+        }
+      );
+
+      const ok: Ok<UpdateOk> = { ok: true };
+      return ok;
+    })
+    .catch((error) => {
+      return {
+        ok: false,
+        error: {
+          code: "io/event_store.update->failed:unknown",
+          innerError: error,
+        },
+      };
+    });
---
+++ src/io/event_store_v2/update/types.ts
@@ -0,0 +1,14 @@
+import { Result } from "../../../";
+
+export type UpdateOk = void;
+
+export type UpdateErr =
+  | { code: "io/event_store.update->failed:unknown" }
+  | { code: "io/event_store.update->failed:item_not_found"; args: any };
+
+export type Update = (args: {
+  idKey: string;
+  idVal: string;
+  event: any;
+  reducer: (state: any, event: any) => any;
+}) => Result<UpdateOk, UpdateErr>;
