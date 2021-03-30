996521ba09aa591a4cf9d20e4f667440e6332378
Marcus Rådell <marcus.radell@sveasolar.com>
2021-03-30 14:10:50 +0200

    Fix io export names.

--- src/io/index.ts
+++ src/io/index.ts
@@ -1,5 +1,5 @@
-export * as EventStoreIo from "./event_store_old";
+export * as OldEventStoreIo from "./event_store_old";
 export * as HttpIo from "./http";
 export * as JwtIo from './jwt';
 export * as ClientIo from './client';
-export * as EventStoreIoV2 from "./event_store"
+export * as EventStoreIo from "./event_store"
